46d092610ba10afeff4b1901238cce16090cbacd
MGuruSwamyTY <56622561+MGuruSwamyTY@users.noreply.github.com>
2020-01-07 18:59:56 +0530

    Add files via upload

---
+++ ehub/src/main/java/com/tyss/ehub/service/BillableService.java
@@ -0,0 +1,18 @@
+package com.tyss.ehub.service;
+
+import java.util.List;
+
+import com.tyss.ehub.dto.Billable;
+
+public interface BillableService {
+
+	public boolean insert(Billable bill);
+
+	public boolean delete(int bId);
+
+	public boolean update(Billable bill);
+
+	public List<Billable> getAllBillable();
+	
+	public List<Billable> getBillable();
+}
